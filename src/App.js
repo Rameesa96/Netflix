79337caf91f4b950a51c4793c0012af02ab21a95
rameesa <websoulrameesa@gmail.com>
2022-11-24 19:57:33 +0530

    commited

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import  React from 'react'
+import Banner from './components/Banner/Banner';
+import Navbar from './components/navbar/navbar';
+import "./App.css"
+import RowPosters from './components/Rowposters/RowPosters';
+import {orginals,action,comedy,horror } from "./components/urls"
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <Banner/>
+     <RowPosters url={orginals} title={"Netflix orginals"}/>
+     <RowPosters url={action} title={"Action"} isSmall/> 
+     <RowPosters url={horror} title={"Horror"} isSmall/> 
+     <RowPosters url={comedy} title={"Comedy"} isSmall/> 
+    
+    </div>
+  );
+}
+
+export default App;
